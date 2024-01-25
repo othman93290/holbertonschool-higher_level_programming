f035d3658e4d01b5945be251148d39cedf65732b
othman93290 <7566@holbertonstudents.com>
2024-01-25 06:00:00 -0800

    4-hidden_discovery.py

---
+++ python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import hidden_4
+    list_names = dir(hidden_4)
+    for name in list_names:
+        if not name [:2] == '__':
+            print("{:s}".format(name))
