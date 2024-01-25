100e8220e13b6c47d465ae1e06a467f7be45a955
othman93290 <7566@holbertonstudents.com>
2024-01-25 06:03:01 -0800

    pycodestyle

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -4,5 +4,5 @@ if __name__ == "__main__":
     import hidden_4
     list_names = dir(hidden_4)
     for name in list_names:
-        if not name [:2] == '__':
+        if not name[:2] == '__':
             print("{:s}".format(name))
