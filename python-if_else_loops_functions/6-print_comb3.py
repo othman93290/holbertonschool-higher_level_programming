1b7b16a591f6cde3b104f4673b90c93dc5141456
othman93290 <7566@holbertonstudents.com>
2024-01-25 05:34:25 -0800

    correction

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(i + 1, 10):
-        print("{:02d}".format(i), end="")
-        print(", {:02d}".format(j), end=", " if j < 9 else "\n")
-
+for i in range(0, 8):
+    for j in range(i, 10):
+        if i != j:
+            print("{:d}{:d}, ".format(i, j), end='')
+print("89")
