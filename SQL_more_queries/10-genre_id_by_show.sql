6736e5aa02fc47dd9a11298b647ba23fc0327d01
othman93290 <7566@holbertonstudents.com>
2024-03-20 13:54:15 +0100

    .\10-genre_id_by_show.sql

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,6 @@
--- Lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+-- TASK: List all shows from the hbtn_0d_tvshows database that have at least one linked genre.
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.tv_show_id
+ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
