048382426ae990b82b29779a48a0f12881d99d2d
othman93290 <7566@holbertonstudents.com>
2024-03-20 13:49:58 +0100

    .\10-genre_id_by_show.sql

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -3,6 +3,7 @@
 -- Les émissions de télévision sont liées à leurs genres correspondants
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.tv_show_id
 -- Trie les résultats par titre de l'émission de télévision et identifiant de genre
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
