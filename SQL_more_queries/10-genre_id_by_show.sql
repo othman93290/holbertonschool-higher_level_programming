3b09cb5754fb08d18099f94e3ebcebfd0396154b
othman93290 <7566@holbertonstudents.com>
2024-03-20 13:51:07 +0100

    .\10-genre_id_by_show.sql

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,9 +1,6 @@
--- Sélectionne le titre des émissions de télévision et les identifiants de genre associés
--- Jointure entre les tables tv_shows et tv_show_genres sur les clés étrangères id et tv_show_id respectivement
--- Les émissions de télévision sont liées à leurs genres correspondants
+-- Lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.tv_show_id
--- Trie les résultats par titre de l'émission de télévision et identifiant de genre
 ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
