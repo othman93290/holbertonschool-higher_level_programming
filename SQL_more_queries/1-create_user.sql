65e2f992f5fc056cb1c076388030446466a3e552
othman93290 <7566@holbertonstudents.com>
2024-03-20 13:30:03 +0100

    .\1-create_user.sql

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,7 +1,4 @@
--- Check if user_0d_1 already exists
-SELECT User FROM mysql.user WHERE User = 'user_0d_1' AND Host = 'localhost' LIMIT 1;
-
--- If user_0d_1 doesn't exist, create it with all privileges
+-- Create MySQL user user_0d_1 with all privileges
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
 -- Grant all privileges to user_0d_1
